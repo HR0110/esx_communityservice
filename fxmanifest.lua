f4ab70890c14eb3fd6a43158a7088abf5ad1a994
HristiyanR <85462760+HR0110@users.noreply.github.com>
2023-08-08 01:15:43 +0300

    Updating to ESXLegacy
    Added bg language;
    Added more options for translating;
    Script is translated to ESXLegacy.

---
+++ fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'ESX Community Service To ESXLegacy'
+
+author 'hristiyan_raykov#0'
+
+version '1.0.0'
+
+shared_scripts {
+	'config.lua',
+	'@es_extended/locale.lua',
+	'locales/en.lua',
+	'locales/bg.lua'
+}
+
+client_scripts {
+	'client/main.lua'
+}
+
+server_scripts {
+	'@oxmysql/lib/MySQL.lua',
+	'server/main.lua'
+}
+
+dependency 'es_extended'
---
+++ locales/en.lua
@@ -0,0 +1,13 @@
+Locales['en'] = {
+	['judge']              	 = 'JUDGE',
+	['escape_attempt']     	 = 'You can not escape. Your community service has been extended.',
+	['remaining_msg']      	 = 'You have ~b~%s~s~ more actions to complete before you can finish your service.',
+	['comserv_msg']        	 = '%s has been sentecned in %s months of community service.',
+	['comserv_finished']   	 = '%s has finished his community service!',
+	['press_to_start']       = 'Press ~INPUT_CONTEXT~ to start.',
+    ['comserv_help']         = 'Give player community service',
+    ['comserv_help_arg1']    = 'Target player Id',
+    ['comserv_help_arg2']    = 'Actions count [suggested: 10-40]',
+    ['endcomserv_help']      = 'Unjail people from jail',
+    ['endcomserv_help_arg1'] = 'Target player Id',
+}
